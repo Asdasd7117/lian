81e2ced9b931d42b46caf9527da4ec812e1ec791
Asdasd7117 <alywm100@gmail.com>
2025-11-10 17:22:54 +0300

    Update build.gradle.kts

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -2,22 +2,20 @@ allprojects {
     repositories {
         google()
         mavenCentral()
-        maven(url = "https://jitpack.io") // ✅ إصلاح الصيغة
+        maven(url = "https://jitpack.io")
+        maven(url = "https://androidx.dev/storage/compose-compiler/repository/")
+        maven(url = "https://maven.pkg.github.com/arthenica/ffmpeg-kit")
     }
 }
 
-val newBuildDir: Directory =
-    rootProject.layout.buildDirectory
-        .dir("../../build")
-        .get()
-rootProject.layout.buildDirectory.value(newBuildDir)
+val newBuildDir = rootProject.layout.buildDirectory
+    .dir("../../build")
+    .get()
+rootProject.layout.buildDirectory.set(newBuildDir)
 
 subprojects {
-    val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
-    project.layout.buildDirectory.value(newSubprojectBuildDir)
-}
-
-subprojects {
+    val newSubprojectBuildDir = newBuildDir.dir(project.name)
+    project.layout.buildDirectory.set(newSubprojectBuildDir)
     project.evaluationDependsOn(":app")
 }
 
