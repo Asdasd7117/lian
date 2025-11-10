a99182ba06b67ff2c4caa76ff731b6d5b4814d0b
Asdasd7117 <alywm100@gmail.com>
2025-11-10 11:56:55 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,28 +1,67 @@
+import 'dart:io';
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(MyImageApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyImageApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false, // لإخفاء العلامة
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('تطبيقي البسيط'),
-          centerTitle: true,
-        ),
-        body: Center(
-          child: Text(
-            'أهلا وسهلا',
-            style: TextStyle(
-              fontSize: 30,
-              fontWeight: FontWeight.bold,
-              color: Colors.blue,
+      debugShowCheckedModeBanner: false,
+      title: 'رفع الصور',
+      home: ImageUploadScreen(),
+    );
+  }
+}
+
+class ImageUploadScreen extends StatefulWidget {
+  @override
+  _ImageUploadScreenState createState() => _ImageUploadScreenState();
+}
+
+class _ImageUploadScreenState extends State<ImageUploadScreen> {
+  File? _image;
+  final picker = ImagePicker();
+
+  Future<void> _pickImage(ImageSource source) async {
+    final pickedFile = await picker.pickImage(source: source);
+    if (pickedFile != null) {
+      setState(() {
+        _image = File(pickedFile.path);
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('رفع الصور إلى الواجهة'),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            _image == null
+                ? Text('لم يتم اختيار صورة بعد')
+                : Image.file(_image!, height: 300),
+            const SizedBox(height: 30),
+            ElevatedButton.icon(
+              onPressed: () => _pickImage(ImageSource.gallery),
+              icon: Icon(Icons.photo),
+              label: Text('اختيار من المعرض'),
             ),
-          ),
+            const SizedBox(height: 15),
+            ElevatedButton.icon(
+              onPressed: () => _pickImage(ImageSource.camera),
+              icon: Icon(Icons.camera_alt),
+              label: Text('التقاط صورة بالكاميرا'),
+            ),
+          ],
         ),
       ),
     );
